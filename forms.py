c1b1df01b0de98385b4cb48af183e5f8c518fb7a
Christopher Alley <calleyviola@gmail.com>
2023-09-05 15:21:34 -0400

    Add registration form

---
+++ forms.py
@@ -0,0 +1,14 @@
+"""Forms for Notes app."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField
+from wtforms.validators import InputRequired, Length, Email
+
+class AddNewUser(FlaskForm):
+    """Form for adding new user"""
+
+    username = StringField("Username", validators=[InputRequired(), Length(max=20)])
+    password = StringField("Password", validators=[InputRequired(), Length(max=30)])
+    email = EmailField("email", validators=[InputRequired(), Length(max=50), Email()])
+    first_name = StringField("First Name", validators=[InputRequired(), Length(max=30)])
+    last_name = StringField("Last Name", validators=[InputRequired(), Length(max=30)])
