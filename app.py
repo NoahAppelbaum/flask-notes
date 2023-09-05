41db3b0b259e58c30c660c756e00e59789d5e07c
Christopher Alley <calleyviola@gmail.com>
2023-09-05 18:09:07 -0400

    Add log out route

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
+"""Flask app for Notes"""
+
 import os
 from flask import Flask, render_template, flash, redirect, jsonify, request, session
 # from flask_debugtoolbar import DebugToolbarExtension
 from models import connect_db, db, User
 # from sqlalchemy.exc import  DataError
-from forms import AddNewUserForm, LoginForm
+from forms import AddNewUserForm, LoginForm, CSRFProtectForm
 
 
-"""Flask app for Cupcakes"""
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "secret"
@@ -16,6 +17,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
 
 
 connect_db(app)
+
+# CSRF_FORM = CSRFProtectForm()
 
 
 @app.get('/')
@@ -31,6 +34,7 @@ def handle_registration():
     form = AddNewUserForm()
 
     if form.validate_on_submit():
+
         user = User.register(
             username = form.username.data,
             password = form.password.data,
@@ -38,6 +42,19 @@ def handle_registration():
             first_name = form.first_name.data,
             last_name = form.last_name.data
         )
+
+        unique_value_conflict = False
+
+        if User.query.filter_by(username=user.username).one_or_none():
+            form.username.errors = ["Username already in use"]
+            unique_value_conflict = True
+
+        if User.query.filter_by(email=user.email).one_or_none():
+            form.email.errors = ["Email already in use"]
+            unique_value_conflict = True
+
+        if unique_value_conflict:
+            return render_template('add_user_form.html', form=form)
 
         db.session.add(user)
         db.session.commit()
@@ -80,8 +97,23 @@ def show_user_info(username):
 
     user = User.query.get_or_404(username)
 
+    form = CSRFProtectForm()
+
     if session.get("username") == username:
-        return render_template("user_details.html", user=user)
+        return render_template("user_details.html", user=user, form=form)
 
     else:
         return redirect("/login")
+
+
+@app.post("/logout")
+def logout_user():
+    """Logout user and redirect to home page"""
+
+    form = CSRFProtectForm()
+
+    if form.validate_on_submit():
+        # Remove "user_id" if present, but no errors if it wasn't
+        session.pop("username", None)
+        flash("Logout successful")
+        return redirect("/")
