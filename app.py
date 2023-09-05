8d3a9fa3267702de0828a9f22de19a4c59d4917a
Noah Appelbaum <noahappelbaum@gmail.com>
2023-09-05 18:03:11 -0500

    fixes post-codereview

--- app.py
+++ app.py
@@ -27,13 +27,31 @@ def redirect_to_register():
 
     return redirect('/register')
 
+#Authorization Routes
+
 @app.route('/register', methods=['GET', 'POST'])
 def handle_registration():
     """Show and handle registration form."""
 
+    #Guard for already logged in
+    if session.get("username"):
+        return redirect(f"/users/{session['username']}")
+
     form = AddNewUserForm()
 
     if form.validate_on_submit():
+        #Guards for duplicate username/email
+        unique_value_conflict = False
+        if User.query.filter_by(username=form.username.data).one_or_none():
+            form.username.errors = ["Username already in use"]
+            unique_value_conflict = True
+
+        if User.query.filter_by(email=form.email.data).one_or_none():
+            form.email.errors = ["Email already in use"]
+            unique_value_conflict = True
+
+        if unique_value_conflict:
+            return render_template('add_user_form.html', form=form)
 
         user = User.register(
             username = form.username.data,
@@ -43,18 +61,7 @@ def handle_registration():
             last_name = form.last_name.data
         )
 
-        unique_value_conflict = False
 
-        if User.query.filter_by(username=user.username).one_or_none():
-            form.username.errors = ["Username already in use"]
-            unique_value_conflict = True
-
-        if User.query.filter_by(email=user.email).one_or_none():
-            form.email.errors = ["Email already in use"]
-            unique_value_conflict = True
-
-        if unique_value_conflict:
-            return render_template('add_user_form.html', form=form)
 
         db.session.add(user)
         db.session.commit()
@@ -71,6 +78,10 @@ def handle_registration():
 @app.route('/login', methods=['GET', 'POST'])
 def handle_login():
     """Show and handle registration form."""
+
+    #Guard for already logged in
+    if session.get("username"):
+        return redirect(f"/users/{session['username']}")
 
     form = LoginForm()
 
@@ -91,6 +102,21 @@ def handle_login():
     return render_template('login_form.html', form=form)
 
 
+@app.post("/logout")
+def logout_user():
+    """Logout user and redirect to home page"""
+
+    form = CSRFProtectForm()
+
+    if form.validate_on_submit():
+        # Remove "user_id" if present, but no errors if it wasn't
+        session.pop("username", None)
+        flash("Logout successful")
+        return redirect("/")
+
+
+#User Routes
+
 @app.get("/users/<username>")
 def show_user_info(username):
     """Shows information to logged-in user"""
@@ -103,17 +129,5 @@ def show_user_info(username):
         return render_template("user_details.html", user=user, form=form)
 
     else:
+        flash("You must be logged in to see user page")
         return redirect("/login")
-
-
-@app.post("/logout")
-def logout_user():
-    """Logout user and redirect to home page"""
-
-    form = CSRFProtectForm()
-
-    if form.validate_on_submit():
-        # Remove "user_id" if present, but no errors if it wasn't
-        session.pop("username", None)
-        flash("Logout successful")
-        return redirect("/")
