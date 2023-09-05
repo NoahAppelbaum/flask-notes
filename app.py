828017033fdfc0ce2e5c65a5e5bf6abd98c242c8
Christopher Alley <calleyviola@gmail.com>
2023-09-05 15:21:20 -0400

    Add route for registration

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
-from flask import Flask, render_template, flash, redirect, jsonify, request
+from flask import Flask, render_template, flash, redirect, jsonify, request, session
 # from flask_debugtoolbar import DebugToolbarExtension
-from models import connect_db, db, DEFAULT_IMAGE_URL
+from models import connect_db, db, User
 # from sqlalchemy.exc import  DataError
-from forms import
-from
+from forms import AddNewUser
+
 
 """Flask app for Cupcakes"""
 app = Flask(__name__)
@@ -16,3 +16,36 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
 
 
 connect_db(app)
+
+
+@app.get('/')
+def redirect_to_register():
+    """Redirect to register page"""
+
+    return redirect('/register')
+
+@app.route('/register', method=['GET', 'POST'])
+def handle_registration():
+    """Show and handle registration form."""
+
+    form = AddNewUser()
+
+    if form.validate_on_submit():
+        user = User.register(
+            username = form.username.data,
+            password = form.password.data,
+            email = form.email.data,
+            first_name = form.first_name.data,
+            last_name = form.last_name.data
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        session["username"] = user.username
+
+        flash(f"Welcome {user.username}")
+        return redirect(f'/users/{user.username}')
+
+    else:
+        return render_template('add_user_form.html', form=form)
