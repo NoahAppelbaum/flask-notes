8d68c18795ec6fa8a30d82e6b9a24e99a7128348
Noah Appelbaum <noahappelbaum@gmail.com>
2023-09-05 13:44:52 -0500

    added user model

--- models.py
+++ models.py
@@ -9,3 +9,35 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+
+class User(db.Model):
+    """model for User"""
+
+    __tablename__ = "users"
+
+    username = db.Column(
+        db.String(20),
+        primary_key=True
+        )
+
+    password = db.Column(
+        db.String(100),
+        nullable=False,
+    )
+
+    email = db.Column(
+        db.String(50),
+        nullable=False,
+        unique=True
+    )
+
+    first_name = db.Column(
+        db.String(30),
+        nullable=False,
+    )
+
+    last_name = db.Column(
+        db.String(30),
+        nullable=False,
+    )
